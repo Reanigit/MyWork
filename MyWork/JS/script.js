03ec1cf994a23870d336d29fc6f6a6694ea56eaf
Reanigit <reanigit@gmail.com>
2021-11-25 13:33:49 +0300

    First commit

---
+++ MyWork/JS/script.js
@@ -0,0 +1,41 @@
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = [
+//     'plum.png', 'orange.jpg', 'apple.bmp'
+// ];
+
+// console.log(arr[2]);
+
+
+
+// alert('Hello');
+// confirm("Are you here");
+// prompt("if you 18", "");
+
+
+
+// const answers = [];
+
+// answers[0] = prompt("wat is you name?", "");
+// answers[1] = prompt("wat is you sure name?", "");
+// answers[2] = prompt("how old is you?", "");
+
+// document.write(answers);
+// typeof(answers);
+
+
+
+// const category = 'toys';
+// console.log(`https//someurl.com/${category}/5`);
+
+// let user = "Ivan";
+// alert(`Hello, ${user}`);
+
+
+
