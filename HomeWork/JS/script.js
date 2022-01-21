c5c2148088a9fcc87dae1a16d3e1c74fd3d0d745
Reanigit <reanigit@gmail.com>
2022-01-21 19:46:45 +0300

    ДЗ Function

--- HomeWork/JS/script.js
+++ HomeWork/JS/script.js
@@ -1,6 +1,17 @@
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
-let personalMovieDB = {
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');  
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+    }
+}
+
+start();
+
+const personalMovieDB = {
     count : numberOfFilms,
     movies : {},
     actors : {},
@@ -8,12 +19,44 @@ let personalMovieDB = {
     privat : false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
- personalMovieDB.movies[a] = b;     
- personalMovieDB.movies[c] = d; 
 
- console.log(personalMovieDB);
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+          if(a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('Done'); 
+          }
+            else {
+                console.log('Error');
+                i--;
+            }
+          
+  
+}
+
+if(personalMovieDB.count <= 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count > 10 && personalMovieDB.count <30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log ('Произошла ошибка');
+}
+ 
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
